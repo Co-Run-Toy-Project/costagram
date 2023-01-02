35e02eb1e19d12853f1596036c8d0b163cbbda9f
dorrion <dhdms8696@naver.com>
2023-01-03 01:59:00 +0900

    [🔨CHORE] 로그를 위한 morgan 설치

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // 설치
 const cors = require('cors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -25,9 +26,13 @@ const connect = async () => {
 
 connect();
 // 미들웨어
+app.use(cors());
 // Content-Type이 application/json인 바디를 파싱하도록 설정
 app.use(bodyParser.json());
-app.use(cors());
+
+// 요청과 응답에 대한 정보를 콘솔에 찍음
+// dev - 개발 / combined - 배포
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('오셨습니까 형님!');
