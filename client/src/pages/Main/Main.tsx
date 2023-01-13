c56b0cad52722ca70f4cf12fa4b262c6dc4b2ff2
yeowool1010 <qyoong3579@gmail.com>
2023-01-14 06:59:38 +0900

    🐛 [Bugfix] top버튼 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -2,7 +2,7 @@ import Navbar from '../../components/Navbar';
 // import PostBox from '../../components/reuse/PostBox';
 import PostModal from '../../components/PostModal';
 // import BoardContainer from '../../components/reuse/BoardContainer';
-// import TopBtn from '../../components/TopBtn';
+import TopBtn from '../../components/TopBtn';
 // import MyPage from '../MyPage/MyPage';
 import RenderPosts from '../../components/RenderPosts';
 
@@ -12,13 +12,10 @@ const Main = () => {
       <PostModal />
       <Navbar />
       <RenderPosts />
-      {/* <div className="mt-[100px] flex justify-center">
-        <PostBox />
-      </div>
+
       <div className="fixed m-5 right-1 bottom-1">
         <TopBtn />
       </div>
-      <BoardContainer /> */}
     </>
   );
 };
