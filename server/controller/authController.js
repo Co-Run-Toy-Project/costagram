b622944e6abbce2aff5952a5f3ed617e8598e3fa
dorrion <dhdms8696@naver.com>
2023-01-15 03:45:19 +0900

    ♻️[refactor] 토큰 받아오기 로직 수정

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,12 +1,18 @@
-const KakaoInstance = require('../utils/kakaoInstance');
+const Axios = require('axios');
 
 exports.getUserInfo = async (req, res, next) => {
   // 인가 코드
+
   const { code } = req.query;
+
   // 토큰 발급
-  const authToken = await KakaoInstance.post(
+  const authToken = await Axios.post(
     'https://kauth.kakao.com/oauth/token',
+    {},
     {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
       params: {
         grant_type: 'authorization_code',
         client_id: process.env.KAKAO_RESTAPI,
@@ -16,17 +22,20 @@ exports.getUserInfo = async (req, res, next) => {
     },
   );
 
-  // Access token을 이용해 정보 가져오기
-  const authInfo = await Axios.post(
-    'https://kapi.kakao.com/v2/user/me',
-    {},
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Bearer ' + authToken.data.access_token,
-      },
-    },
-  );
-  //동의항목을 설정한 사용자의 정보 보냄
-  res.status(200).json(authInfo.data);
+  // 토큰 받아오기 구현
+  console.log(authToken.data.access_token);
+  // // Access token을 이용해 정보 가져오기
+  // const authInfo = await Axios.post(
+  //   'https://kapi.kakao.com/v2/user/me',
+  //   {},
+  //   {
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //       Authorization: 'Bearer ' + authToken.data.access_token,
+  //     },
+  //   },
+  // );
+  // // 동의항목을 설정한 사용자의 정보 보냄
+  // console.log(authToken.data);
+  // res.status(200).json(authToken.data);
 };
