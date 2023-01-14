41bbeea9d9b9902ee90ebe82b10d65b8ebec7161
hun0613 <pshun7614@gmail.com>
2023-01-14 16:21:00 +0900

    ✨[feat] oauth 인가코드 백엔드 전달로직 구현

---
+++ client/src/apis/post/get/getOauth.tsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const getOauth = async (permissionCode: string | null) => {
+    return axios
+        .get(`/oauth/kakao/callback?code=${permissionCode}`, {
+            baseURL: process.env.REACT_APP_BASE_URL,
+            headers: {
+                withCredentials: true,
+                'Content-Type': `application/json`,
+            },
+        })
+};
+
+export default getOauth;
