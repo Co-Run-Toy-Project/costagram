675315355b190151b51ee94c1c28e4b9da72d1a6
dorrion <dhdms8696@naver.com>
2023-01-14 12:18:55 +0900

    ✨[feat] 게시물 1개 조회 컨트롤러 구현

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -26,6 +26,21 @@ exports.updatePost = async (req, res) => {
     });
 };
 
+exports.getOnePost = async (req, res, next) => {
+  const { postId } = req.params;
+
+  // postId에 해당하는 데이터 1개 찾기
+  Post.findOne({ postId })
+    .then(posts => {
+      // 클라이언트로 전송
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      // 실패 시 에러 전달
+      res.status(500).send(err);
+    });
+};
+
 exports.getAllPost = async (req, res, next) => {
   // find가 없으면 모든 데이터 조회
   Post.find({})
