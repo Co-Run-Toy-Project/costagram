241be7970ff275f794085c9deaf3210b80f88e82
rachelyu1025 <rachelyu.1025@gmail.com>
2023-01-14 11:07:26 +0900

    ✨ [Feat] delete 요청에 대한 로직 작성중 , eslint 임시 수정

--- client/src/hooks/post/useDeletePost.tsx
+++ client/src/hooks/post/useDeletePost.tsx
@@ -1,8 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import deletePost from '../../apis/post/deletePost';
 
 const useDeletePost = () => {
+  // const queryClient = useQueryClient();
+
   return useMutation(deletePost);
 };
 
 export default useDeletePost;
+
+// {
+//   onSuccess: () => {
+//     queryClient.invalidateQueries(['get/post']);
+//   },
+// }
--- client/src/hooks/post/useGetPost.tsx
+++ client/src/hooks/post/useGetPost.tsx
@@ -1,7 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import getPosts from '../../apis/post/getPosts';
 
 const useGetPosts = () => {
+  const queryClient = useQueryClient();
+  queryClient.invalidateQueries(['get/post']);
+
   return useQuery(['get/post'], () => getPosts(), {
     enabled: false,
   });
