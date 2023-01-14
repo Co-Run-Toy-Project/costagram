b82f4862e2c985c39dbfc07fd3b67aa93272e6c4
dorrion <dhdms8696@naver.com>
2023-01-14 13:18:22 +0900

    ♻️[refactor] 댓글 등록 컨트롤러 수정
    - 원본 게시물이 있을 때만 가능

--- server/controller/commentController.js
+++ server/controller/commentController.js
@@ -4,23 +4,29 @@ const Comment = require('../models/schema/comment');
 exports.addComment = async (req, res, next) => {
   const { postId } = req.params;
   const { commentContent } = req.body;
-  const newComment = new Comment({
-    commentContent,
-  });
 
   const message = { message: '댓글 등록이 완료되었습니다!' };
+  const post = Post.findOne({ postId });
+  // 게시물이 있는지 확인
+  if (post) {
+    const newComment = new Comment({ postId, commentContent });
+    await newComment.save();
 
-  await newComment.save();
-  Post.findOneAndUpdate({ postId }, { $push: { comments: newComment } })
-    .then(() => res.status(200).json(message))
-    .catch(err => {
-      res.status(500).send(err);
-    });
+    await Post.findOneAndUpdate({ postId }, { $push: { comments: newComment } })
+      .then(() => {
+        res.status(200).json(message);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  } else {
+    res.status(500).send({ message: '게시물이 존재하지 않습니다!' });
+  }
 };
 
 // 댓글 삭제
 exports.deleteComment = async (req, res) => {
-  const { postId, commentId } = req.params;
+  const { commentId } = req.params;
   const message = { message: `${commentId}번 댓글이 삭제되었습니다!` };
   await Comment.deleteOne({ commentId })
     .then(() => res.status(200).json(message))
