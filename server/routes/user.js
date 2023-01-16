ca6d56cf448cfda8a0ca31df315a334494fbf25d
Oheun Kim <dhdms8696@naver.com>
2023-01-16 19:54:35 +0900

    ✨[feat] 토큰 수정 및 프로필 컨트롤러 구현 (#66)
    * ✨[feat] 토큰 권한 확인 로직 추가
    
    - 회원가입 시 프로필 이미지 추가
    
    * ✨[feat] 유저 프로필 조회 구현
    
    * ✨[feat] 프로필 소개글 수정 구현

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,44 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// 컨트롤러 불러오기
+const userController = require('../controller/userController');
+
 // 프로필 조회
-router.get('/', (req, res) => {
-  res.status(200).json({
-    userId: 1,
-    userName: 'oheun',
-    profileImage:
-      'https://images.unsplash.com/photo-1470093851219-69951fcbb533?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    introduce: 'api 문서 만드는 중',
-    userPosts: [
-      {
-        postId: 1,
-        imagePath:
-          'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80',
-        likeCount: 1,
-        commentCount: 12,
-      },
-      {
-        postId: 2,
-        imagePath:
-          'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80',
-        likeCount: 3,
-        commentCount: 2,
-      },
-      {
-        postId: 3,
-        imagePath:
-          'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80',
-        likeCount: 5,
-        commentCount: 1,
-      },
-    ],
-    userPostsCount: 3,
-  });
-});
+router.get('/', userController.getUser);
 
 // 프로필 수정
-router.patch('/', (req, res) => {
-  res.status(200).json({ message: '프로필 수정 success' });
-});
+router.patch('/', userController.updateUser);
 
 module.exports = router;
