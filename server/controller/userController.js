3e8801f28fab0a877a6578c6265123583f813bd1
dorrion <dhdms8696@naver.com>
2023-01-16 18:06:19 +0900

    ✨[feat] 프로필 소개글 수정 구현

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -14,3 +14,17 @@ exports.getUser = async (req, res, next) => {
       res.status(500).send(err);
     });
 };
+
+exports.updateUser = async (req, res) => {
+  // 토큰에 해당하는 유저 정보 찾아 수정하기
+  // 아직 프론트에서 토큰이 통용되지 않은 것 같아 김오은 정보 찾아 보내드리겠습니다
+  const update = { introduce: req.body.introduce };
+  const message = { message: '소개글 수정이 완료되었습니다!' };
+  await User.findOneAndUpdate({ _id: '63c5107536dbdf1d1d5e4ee3' }, update)
+    .then(() => {
+      res.status(200).json(message);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // 컨트롤러 불러오기
-const authController = require('../controller/authController');
 const userController = require('../controller/userController');
 
 // 프로필 조회
 router.get('/', userController.getUser);
 
 // 프로필 수정
-router.patch('/', (req, res) => {
-  res.status(200).json({ message: '프로필 수정 success' });
-});
+router.patch('/', userController.updateUser);
 
 module.exports = router;
