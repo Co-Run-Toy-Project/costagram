b05600064459cbe2445a8afe427a293fb641c3a9
inside-eun <dameun0808@gmail.com>
2023-01-19 00:56:20 +0900

    ✨FEAT : 날씨 API 수정

--- client/src/apis/weather/getWeather.tsx
+++ client/src/apis/weather/getWeather.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const getCurrWeather = async () => {
+const getCurrWeather = async (lat: number, lon: number) => {
   const weatherAPIKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const lat = 37.265974;
-  const lon = 126.999874;
+  // const lat = 37.265974;
+  // const lon = 126.999874;
 
   return axios
     .get(`/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`, {
