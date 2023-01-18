1b712e2d79ee1bca3a512801062b0e9f0a01d988
yeowool1010 <qyoong3579@gmail.com>
2023-01-19 08:54:47 +0900

    🐛 [Fix] 무한스크롤 에러 수정

--- client/src/components/RenderPosts.tsx
+++ client/src/components/RenderPosts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Loading from './Loading';
 import useGetPosts from '../hooks/posts/useGetPost';
@@ -6,26 +6,11 @@ import PostBox from './reuse/PostBox';
 
 const RenderPosts = () => {
   const { data, error, refetch, status } = useGetPosts();
-  const [isNextPage, setIsNextPage] = useState(false);
+
   useEffect(() => {
     refetch();
   }, []);
 
-  // 스크롤을 감지하여 다음 데이터를 불러오는 boolean값을 설정
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      const scrollable =
-        document.documentElement.scrollHeight - window.innerHeight + 25;
-      const scrolled = Math.ceil(window.pageYOffset);
-      console.log(scrollable);
-      console.log(scrolled);
-      if (scrolled === scrollable) {
-        setIsNextPage(true);
-      } else if (scrolled !== scrollable) {
-        setIsNextPage(false);
-      }
-    });
-  });
   // 데이터로드 되기 전의 로딩 스피너
   if (status === 'loading') return <Loading />;
 
@@ -37,33 +22,60 @@ const RenderPosts = () => {
         {`${error}` as string}
       </div>
     );
+
   const resData = data?.data;
+  console.log(typeof data);
+  console.log(Array.isArray(resData));
+
+  const [itemIndex, setItemIndex] = useState(0);
+
+  // 초기값 2개만 저장
+  const [post, setPost] = useState(resData.slice(0, 2));
+  // 로딩스피너 boolean  state
+  // const [isLoding, setIsLoding] = useState(false);
+
+  // scrollTop과 clientHeight를 더한 값이 scrollHeight 값과 같을 때, 즉 스크롤이 화면 맨 끝에 도달했을 때, 다음 5개의 데이터를 렌더링
+  const infiniteScroll = useCallback(() => {
+    // setIsLoding(true);
+
+    const scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight,
+    );
+    const scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop,
+    );
+    let clientHeight = 0;
+
+    clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight === scrollHeight) {
+      setItemIndex(itemIndex + 20);
+      setPost(post.concat(resData.slice(itemIndex + 5, itemIndex + 10)));
+      // setIsLoding(false);
+    }
+  }, [itemIndex, post]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', infiniteScroll, true); // 스크롤 이벤트 등록
+    return () => window.removeEventListener('scroll', infiniteScroll, true); // 스크롤 이벤트 삭제(컴포넌트 언마운트시)
+  }, [infiniteScroll]);
 
   return (
-    <div className="overflow-y-auto h-fit">
-      <InfiniteScroll
-        className="overflow-y-auto h-fit"
-        // 데이터를 불러오는 함수
-        next={() => refetch()}
-        // 다음 페이지를 불러오는가를 정하는 boolean값 true:요청O false:요청X
-        hasMore={isNextPage}
-        // 로딩 문구&스피너
-        loader={<Loading />}
-        // 몇 개의 데이터를 나열 할 것인가
-        dataLength={resData ? resData.length : 0}
-      >
-        <div>
-          {/* post map으로 나열되는 부분 */}
-          {resData &&
-            resData.map((el: any) => {
-              return (
-                <div key={el.postId} className="flex justify-center mt-10">
-                  <PostBox key={el.postId} data={el} />
-                </div>
-              );
-            })}
-        </div>
-      </InfiniteScroll>
+    <div className="">
+      <div>
+        {/* post map으로 나열되는 부분 */}
+        {post &&
+          post.map((el: any) => {
+            return (
+              <div key={el.postId} className="flex justify-center mt-10">
+                <PostBox key={el.postId} data={el} />
+              </div>
+            );
+          })}
+        {/* {isLoding && <Loading />} */}
+      </div>
     </div>
   );
 };
