bd63eb6da8852db4730dc73bc299d01ce4b96dfa
dorrion <dhdms8696@naver.com>
2023-01-19 01:13:13 +0900

    🎨[structure] 프로필 조회 수정
    - 토큰에 맞게 유저 정보 수정

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,9 +2,14 @@
 const User = require('../models/schema/user');
 
 exports.getUser = async (req, res, next) => {
+  // 또 어디서 userId가 오고 있네
+
+  let userCheck = await User.findOne({
+    userName: req.tokenInfo,
+  });
+
   // 토큰에 해당하는 유저 정보 찾기
-  // 아직 프론트에서 토큰이 통용되지 않은 것 같아 김오은 정보 찾아 보내드리겠습니다
-  User.findOne({ _id: '63c649c29279581609e2d175' })
+  User.findOne({ userName: userCheck.userName })
     .then(posts => {
       // 클라이언트로 전송
       res.status(200).json(posts);
