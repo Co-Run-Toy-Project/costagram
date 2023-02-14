075477f481b904161ab151c63f2d7dfa400791a6
Oheun Kim <dhdms8696@naver.com>
2023-02-14 14:55:12 +0900

    ✨[feat] 이미지를 S3에 업로드하고 링크를 반환한다 (#113)
    * ✨[feat] S3 업로드 함수
    
    * ✨[feat] multer 처리 및 라우터 수정
    
    * ✨[feat] 이미지 배열 저장
    
    * ✨[feat] formdata로 수정

--- client/src/hooks/posts/useCreatePost.tsx
+++ client/src/hooks/posts/useCreatePost.tsx
@@ -12,12 +12,27 @@ interface FormData {
 
 const apis = {
   postImageAndContent: async (data: FormData) => {
+    // FormData 객체 생성
+    const formdata = new FormData();
+
+    // formData 추가하기
+    formdata.append('postContent', data.postContent);
+    formdata.append('weather', data.weather);
+    formdata.append('location', data.location);
+
+    // 객체여서 배열로 바꾸기
+    const imagePathArray = Object.values(data.imagePath);
+
+    imagePathArray.forEach((image: any) => {
+      formdata.append(`imagePath`, image);
+    });
+
     return await axios
-      .post(`/post`, data, {
+      .post(`/post`, formdata, {
         baseURL: process.env.REACT_APP_BASE_URL,
         headers: {
           Authorization: `${localStorage.getItem('token')}`,
-
+          'Content-Type': 'multipart/form-data',
         },
       })
       .catch(err => console.log(err));
