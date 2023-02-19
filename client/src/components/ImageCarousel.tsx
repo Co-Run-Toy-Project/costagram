83596e2a31bc5f3db9eb5e6d41b7372d4240af23
dorrion <dhdms8696@naver.com>
2023-02-20 03:38:30 +0900

    ✨[feat] 이미지 캐러셀 구현

---
+++ client/src/components/ImageCarousel.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+interface ImageCarouselProps {
+  imagePath: string[];
+}
+
+const ImageCarousel: React.FC<ImageCarouselProps> = ({ imagePath }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentIndex((currentIndex - 1 + imagePath.length) % imagePath.length);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % imagePath.length);
+  };
+
+  const imageWidth = 470; // change this value to adjust the width of each image
+  const carouselWidth = imagePath.length * imageWidth;
+  const offset = -currentIndex * imageWidth;
+
+  return (
+    <div className="relative overflow-hidden h-[470px]">
+      <div
+        className="absolute top-0 left-0 w-full transition-transform duration-500"
+        style={{ transform: `translateX(${offset}px)` }}
+      >
+        {imagePath.map((image, index) => (
+          <img
+            key={index}
+            className="inline-block"
+            src={image}
+            alt="carousel"
+            width={imageWidth}
+          />
+        ))}
+      </div>
+      <button
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 px-4 py-2 bg-black text-white rounded-r-md"
+        onClick={handlePrevClick}
+        type="button"
+      >
+        Prev
+      </button>
+      <button
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 px-4 py-2 bg-black text-white rounded-l-md"
+        onClick={handleNextClick}
+        type="button"
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default ImageCarousel;
