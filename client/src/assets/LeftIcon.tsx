a462a157c7228ee8034d65302d1cb865550f1a0f
dorrion <dhdms8696@naver.com>
2023-02-21 22:42:44 +0900

    💄[ui] 아이콘 굵기 및 색깔 변경

--- client/src/assets/LeftIcon.tsx
+++ client/src/assets/LeftIcon.tsx
@@ -4,8 +4,8 @@ const LeftIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
+      strokeWidth="4"
+      stroke="black"
       className="w-3 h-3"
     >
       <path
