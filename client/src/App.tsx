42a747348298f4f42a29cc384fd9366cae3b21cd
rachelyu1025 <rachelyu.1025@gmail.com>
2023-03-23 17:21:04 +0900

    ✨ [Feat] react-helmet-async 를 이용한 meta tag 작성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,28 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Router from './router/Router';
+import MetaTag from './components/reuse/SEOMetaTag';
 
 const App: React.FC = () => {
   const queryClient = new QueryClient();
 
+  const mainMeta = {
+    title: 'Costagram',
+    description: 'Instagram 서비스를 바탕으로 구현한 소셜 웹서비스',
+    url: 'favicon.ico',
+    imagesrc: 'Costagram.png',
+    keywords: 'Instagram, Costgram, SNS',
+  };
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Router />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <MetaTag item={mainMeta} />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Router />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   );
 };
 
